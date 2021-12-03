2d1f59ac81a74fa2f665bfc5f4613bd22b3b9b25
Matthew Bunday <mkbunday@gmail.com>
2021-12-02 20:52:31 -0500

    Auction timing info

---
+++ pages/api/auction.ts
@@ -0,0 +1,24 @@
+import { ethers } from 'ethers';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { provider } from '../../lib/provider'
+
+type Data = {
+  value: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+
+  const currentBlock = await provider.getBlockNumber();
+
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=15, stale-while-revalidate=30'
+  )
+
+  res.status(200).json({
+    value: 13770188 - currentBlock,
+  })
+}
