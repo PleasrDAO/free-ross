fcb7a0aaa808599574115e95ad80e393260bcd1b
Matthew Bunday <mkbunday@gmail.com>
2021-12-07 18:31:02 -0500

    Experiment with Redis

--- pages/api/treasury.ts
+++ pages/api/treasury.ts
@@ -1,24 +1,53 @@
-import { ethers } from 'ethers';
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { provider } from '../../lib/provider'
+import { ethers, BigNumber } from "ethers";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { provider } from "../../lib/provider";
+import Redis from "ioredis";
 
 type Data = {
-  value: string
-}
+  value: string;
+  bid: string;
+};
+
+const fmt = (bn: BigNumber) =>
+  ethers.utils.commify(ethers.utils.formatUnits(bn.sub(bn.mod(1e15))));
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const redis = new Redis(process.env.REDIS_URL);
 
-  const balance = await provider.getBalance("0xc102d2544a7029f7BA04BeB133dEADaA57fDF6b4");
+  const [last_bid, last_bal, high_bal] = (
+    await redis.mget("last_bid", "last_bal", "high_bal")
+  ).map((v) => BigNumber.from(v));
+
+  const balance = await provider.getBalance(
+    "0xc102d2544a7029f7BA04BeB133dEADaA57fDF6b4"
+  );
+
+  let display_bal;
+  let bid = last_bid;
+
+  if (balance.gte(high_bal)) {
+    redis.set("high_bal", balance.toString());
+    display_bal = balance;
+  } else {
+    display_bal = balance.add(last_bid);
+  }
+
+  if (balance.lt(last_bal)) {
+    bid = last_bal.sub(balance);
+    redis.set("last_bid", bid.toString());
+  }
+  redis.set("last_bal", balance.toString());
 
   res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=15, stale-while-revalidate=30'
-  )
+    "Cache-Control",
+    "public, s-maxage=15, stale-while-revalidate=30"
+  );
 
   res.status(200).json({
-    value: ethers.utils.commify(ethers.utils.formatUnits(balance.sub(balance.mod(1e15)))),
-  })
+    value: fmt(display_bal),
+    bid: fmt(bid),
+  });
 }
