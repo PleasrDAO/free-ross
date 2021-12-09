725a429f5a57bd3d9312c56e5d269bc980b12a86
Matthew Bunday <mkbunday@gmail.com>
2021-12-09 04:42:12 -0500

    Remove Redis

--- pages/api/treasury.ts
+++ pages/api/treasury.ts
@@ -1,7 +1,6 @@
 import { ethers, BigNumber } from "ethers";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { provider } from "../../lib/provider";
-import Redis from "ioredis";
 
 type Data = {
   value: string;
@@ -15,41 +14,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const redis = new Redis(process.env.REDIS_URL);
-
-  const [last_bid, last_bal, high_bal] = (
-    await redis.mget("last_bid", "last_bal", "high_bal")
-  ).map((v) => BigNumber.from(v));
 
   const balance = await provider.getBalance(
     "0xc102d2544a7029f7BA04BeB133dEADaA57fDF6b4"
   );
 
-  let display_bal;
-  let bid = last_bid;
-
-  if (balance.gte(high_bal)) {
-    redis.set("high_bal", balance.toString());
-    display_bal = balance;
-  } else {
-    display_bal = balance.add(last_bid);
-  }
-
-  if (balance.lt(last_bal)) {
-    bid = last_bal.sub(balance);
-    redis.set("last_bid", bid.toString());
-  }
-  redis.set("last_bal", balance.toString());
+  let display_bal = balance;
 
   res.setHeader(
     "Cache-Control",
     "public, s-maxage=15, stale-while-revalidate=30"
   );
 
-  bid = bid.mul(100).div(103);
-
   res.status(200).json({
     value: fmt(display_bal),
-    bid: fmt(bid),
   });
 }
